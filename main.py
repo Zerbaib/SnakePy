99eb4fef309dfab4546e26a9a1fa15c32d1d0eda
Hugo Hennetin <hugo.hennetin@proton.me>
2024-04-26 12:31:00 +0200

    restart

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-from tkinter import *
-from random import *
-
-game = Tk() # creation d'une fenetre avec tk
-game.title("Snake game") # on met sont nom
-
-hauteur = game.winfo_screenheight() # on recuperre les donner de l'ecran de l'utilisateur
-largeur = game.winfo_screenwidth()
-H = str(int(hauteur/1.1)) # on met les dimensions qui vas a la taille de l'ecran
-L = str(int(largeur/2))
-game.geometry(f"{L}x{H}+0+0")
-
-game.mainloop() # on laisse tourner la fenettre pour le jeux
+from turtle import *
+from random import randrange
+from freegames import square, vector
